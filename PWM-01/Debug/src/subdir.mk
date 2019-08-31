415ec305cce68b32d53f604ee6f171adee36c1b4
wk415937 <wk415937@gmail.com>
2019-08-31 20:54:24 +0800

    Add Files

---
+++ PWM-01/Debug/src/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/_write.c \
+../src/delay.c \
+../src/led_init.c \
+../src/main.c 
+
+OBJS += \
+./src/_write.o \
+./src/delay.o \
+./src/led_init.o \
+./src/main.o 
+
+C_DEPS += \
+./src/_write.d \
+./src/delay.d \
+./src/led_init.d \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU ARM Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DUSE_FULL_ASSERT -DSTM32F10X_MD -DUSE_STDPERIPH_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f1-stdperiph" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ PWM-01/include/led_init.h
@@ -0,0 +1,7 @@
+#ifndef LED_INIT_H_
+#define LED_INIT_H_
+
+extern
+void led_init(void);
+
+#endif // LED_INIT_H_
